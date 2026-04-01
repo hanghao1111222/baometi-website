feb9d0c932ef7d4e74a2c933ecf48d3a48bd4a8b
hanghao1111222 <1845336570@qq.com>
2026-04-01 16:16:11 +0800

    Init commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-
-function getPlugins() {
-  const plugins = [react(), tsconfigPaths()];
-  return plugins;
-}
-
 export default defineConfig({
-  plugins: getPlugins(),
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 5000,
     host: '0.0.0.0',
-    hmr: {
-      overlay: true,
-      path: '/hot/vite-hmr',
-      port: 6000,
-      clientPort: 443,
-      timeout: 30000,
-    },
   },
 });
